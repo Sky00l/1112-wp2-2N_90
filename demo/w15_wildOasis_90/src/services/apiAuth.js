a93965f80211bff5ff81fc783e977ed08ef0ac32
sky00l <207410290@gmail.com>
2024-06-05 21:47:13 +0800

    upload final answer

--- demo/w15_wildOasis_90/src/services/apiAuth.js
+++ demo/w15_wildOasis_90/src/services/apiAuth.js
@@ -1,30 +1,28 @@
-import supabase, { supabaseUrl } from "./supabase";
+import supabase, { supabaseUrl } from './supabase';
 
-export const login = async({ email, password }) => {
-    console.log('login email password', email, password);
-    let { data, error } = await supabase.auth.signInWithPassword({
-        email,
-        password,
-    })
-    if (error) throw new Error(error.message);
+export const login = async ({ email, password }) => {
+  console.log('login email password', email, password);
+  let { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+  if (error) throw new Error(error.message);
 
-    console.log('login data', data)
-    return data;
-}
+  console.log('login data', data);
+  return data;
+};
 
-export const getCurrentUser = async() => {
-    const { data: session } = await supabase.auth.getSession();
-    if (!session.session) return null;
+export const getCurrentUser = async () => {
+  const { data: session } = await supabase.auth.getSession();
+  if (!session.session) return null;
 
-    const { data, error } = await supabase.auth.getUser();
-    console.log('getCurrentUser data', data);
-    if (error) throw new Error(error.message);
-    return data.user;
+  const { data, error } = await supabase.auth.getUser();
+  console.log('getCurrentUser data', data);
+  if (error) throw new Error(error.message);
+  return data.user;
+};
 
-
-}
-
-export const logout = async() => {
-    const { error } = await supabase.auth.signOut();
-    if (error) throw new Error(error.message);
-}
+export const logout = async () => {
+  const { error } = await supabase.auth.signOut();
+  if (error) throw new Error(error.message);
+};
